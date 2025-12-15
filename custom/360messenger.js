b6651234c1357c3bfe104422db819d142d0df0aa
hashem sakkaki <alborzsite@gmail.com>
2025-12-15 12:46:17 +0330

    all ok

--- custom/360messenger.js
+++ custom/360messenger.js
@@ -20,6 +20,12 @@ class messenger360 extends NotificationProvider {
             };
             config = this.getAxiosConfigWithProxy(config);
 
+            // Use custom template if enabled
+            let message = msg;
+            if (notification.messenger360UseTemplate && notification.messenger360Template) {
+                message = this.applyTemplate(notification.messenger360Template, msg, monitorJSON, heartbeatJSON);
+            }
+
             const hasGroupId = notification.messenger360GroupId && notification.messenger360GroupId.trim() !== "";
             const hasRecipient = notification.messenger360Recipient && notification.messenger360Recipient.trim() !== "";
 
@@ -28,14 +34,14 @@ class messenger360 extends NotificationProvider {
                 // Send to individual recipient
                 let recipientData = {
                     "phonenumber": notification.messenger360Recipient,
-                    "text": msg,
+                    "text": message,
                 };
                 await axios.post("https://api.360messenger.com/v2/sendMessage", recipientData, config);
 
                 // Send to group
                 let groupData = {
                     "groupId": notification.messenger360GroupId,
-                    "text": msg,
+                    "text": message,
                 };
                 await axios.post("https://api.360messenger.com/v2/sendGroup", groupData, config);
 
@@ -45,7 +51,7 @@ class messenger360 extends NotificationProvider {
             else if (hasGroupId) {
                 let data = {
                     "groupId": notification.messenger360GroupId,
-                    "text": msg,
+                    "text": message,
                 };
                 await axios.post("https://api.360messenger.com/v2/sendGroup", data, config);
                 return okMsg + " (Sent to group)";
@@ -54,7 +60,7 @@ class messenger360 extends NotificationProvider {
             else if (hasRecipient) {
                 let data = {
                     "phonenumber": notification.messenger360Recipient,
-                    "text": msg,
+                    "text": message,
                 };
                 await axios.post("https://api.360messenger.com/v2/sendMessage", data, config);
                 return okMsg + " (Sent to recipient)";
@@ -65,6 +71,40 @@ class messenger360 extends NotificationProvider {
 
         } catch (error) {
             this.throwGeneralAxiosError(error);
+        }
+    }
+
+    /**
+     * Apply template with variables
+     * @param {string} template - Template string
+     * @param {string} msg - Default message
+     * @param {object} monitorJSON - Monitor data
+     * @param {object} heartbeatJSON - Heartbeat data
+     * @returns {string} Formatted message
+     */
+    applyTemplate(template, msg, monitorJSON, heartbeatJSON) {
+        try {
+            // Simple template replacement
+            let result = template;
+            
+            // Replace monitor variables
+            if (monitorJSON) {
+                result = result.replace(/{{ monitorJSON\['name'\] }}/g, monitorJSON.name || '');
+                result = result.replace(/{{ monitorJSON\['url'\] }}/g, monitorJSON.url || '');
+            }
+            
+            // Replace message variable
+            result = result.replace(/{{ msg }}/g, msg);
+            
+            // Handle conditional blocks (simple if statements)
+            result = result.replace(/{% if monitorJSON %}([\s\S]*?){% endif %}/g, (match, content) => {
+                return monitorJSON ? content : '';
+            });
+            
+            return result;
+        } catch (error) {
+            // If template parsing fails, return original message
+            return msg;
         }
     }
 
--- custom/360messenger.vue
+++ custom/360messenger.vue
@@ -26,7 +26,7 @@
         <div class="form-text">{{ $t("wayToWrite360messengerRecipient", ["00117612345678", "00117612345678@s.whatsapp.net", "123456789012345678@g.us"]) }}</div>
     </div>
 
-    <!-- Switch to enable/disable Combobox -->
+    <!-- Checkbox to enable/disable Combobox -->
     <div class="mb-3 form-check form-switch">
         <input 
             id="360messenger-enable-options" 
@@ -39,7 +39,7 @@
         </label>
     </div>
 
-    <!-- Combobox that is enabled/disabled based on Switch -->
+    <!-- Combobox that is enabled/disabled based on Checkbox -->
     <div class="mb-3">
         <label for="360messenger-group-list" class="form-label">Group Chat</label>
         <select 
@@ -67,14 +67,39 @@
     <i18n-t tag="div" keypath="More info on:" class="mb-3 form-text">
         <a href="https://360messenger.com/" target="_blank">https://360messenger.com/</a>
     </i18n-t>
+
+    <div class="mb-3">
+        <div class="form-check form-switch">
+            <input v-model="$parent.notification.messenger360UseTemplate" class="form-check-input" type="checkbox">
+            <label class="form-check-label">{{ $t("Use Custom Message Template") }}</label>
+        </div>
+
+        <div class="form-text">
+            {{ $t("Enable custom message formatting with variables") }}
+        </div>
+    </div>
+
+    <template v-if="$parent.notification.messenger360UseTemplate">
+        <div class="mb-3">
+            <label class="form-label" for="360messenger-template">{{ $t('Message Template') }}</label>
+            <TemplatedTextarea 
+                id="360messenger-template" 
+                v-model="$parent.notification.messenger360Template" 
+                :required="true" 
+                :placeholder="messenger360TemplatedTextareaPlaceholder"
+            ></TemplatedTextarea>
+        </div>
+    </template>
 </template>
 
 <script>
 import HiddenInput from "../HiddenInput.vue";
+import TemplatedTextarea from "../TemplatedTextarea.vue";
 
 export default {
     components: {
         HiddenInput,
+        TemplatedTextarea,
     },
     data() {
         return {
@@ -84,8 +109,19 @@ export default {
             errorMessage: ""
         };
     },
+    computed: {
+        messenger360TemplatedTextareaPlaceholder() {
+            return this.$t("Example:", [
+                `
+360Messenger Alert{% if monitorJSON %} - {{ monitorJSON['name'] }}{% endif %}
+
+{{ msg }}
+                `,
+            ]);
+        }
+    },
     watch: {
-        // When switch is enabled, fetch groups from API
+        // When checkbox is enabled, fetch groups from API
         isOptionsEnabled(newValue, oldValue) {
             if (newValue) {
                 this.fetchGroups();
@@ -145,3 +181,9 @@ export default {
     }
 };
 </script>
+
+<style lang="scss" scoped>
+textarea {
+    min-height: 150px;
+}
+</style>
